8d9dedf12145b096eeadce2ab45901f4a828b80f
14Kay <619113277@qq.com>
2024-12-21 18:52:52 +0800

    ico 文件 编译问题

--- src/shell/computed.sh
+++ src/shell/computed.sh
@@ -2,7 +2,7 @@
 echo 'export default {
   items: [' > ./static/scripts/config.js
 
-find meme -type f -print0 | grep -z -i -E '\.(jpg|png|jfif|webp|gif|jpeg|bmp)$' | sort -z | while IFS= read -r -d '' file_name
+find meme -type f -print0 | grep -a -z -i -E '\.(jpg|png|jfif|webp|gif|jpeg|bmp)$' | sort -z | while IFS= read -r -d '' file_name
 do
     echo -n '    ' >> ./static/scripts/config.js
     echo $(jq -R -c . <<< "$file_name")',' >> ./static/scripts/config.js
